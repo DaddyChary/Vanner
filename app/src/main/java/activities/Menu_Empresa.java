a0f59c67f5d3a51d202424d0f81d3fc7e14ff07d
LuisTobar765 <latobarg@gmail.com>
2024-11-13 20:33:55 -0300

    se agregaron los activities de menu empresa y entrenador

--- app/src/main/java/activities/Menu_Empresa.java
+++ app/src/main/java/activities/Menu_Empresa.java
@@ -1,17 +1,54 @@
 package activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.socialab2.R;
+import com.google.android.material.button.MaterialButton;
 
 public class Menu_Empresa extends AppCompatActivity {
+
+    // Definimos los botones
+    private MaterialButton btnEditarPerfilEntrenador;
+    private MaterialButton btnMensaje;
+    private MaterialButton btnVolver;
+    private MaterialButton btnCerrarseccion;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.menu_empresa);
+
+        btnEditarPerfilEntrenador = findViewById(R.id.btnEditarPerfilEntrenador);
+        btnMensaje = findViewById(R.id.btnMensaje);
+        btnVolver = findViewById(R.id.btnVolver);
+        btnCerrarseccion = findViewById(R.id.btnCerrarseccion);
+
+        btnEditarPerfilEntrenador.setOnClickListener(v -> abrirPerfilEntrenador());
+        btnVolver.setOnClickListener(v -> volverAInicio());
+        btnCerrarseccion.setOnClickListener(v -> cerrarSeccion());
+    }
+
+    private void abrirPerfilEntrenador() {
+        Intent intent = new Intent(Menu_Empresa.this, Menu_entrenador.class);
+        startActivity(intent);
+    }
+
+    /*private void abrirMensajes() {
+        // Aquí va la lógica para abrir la actividad de mensajes
+        Intent intent = new Intent(Menu_Empresa.this, MensajesActivity.class);
+        startActivity(intent);
+    }*/
+
+    private void volverAInicio() {
+        Intent intent = new Intent(Menu_Empresa.this, MainActivity.class);
+        startActivity(intent);
+    }
+
+    private void cerrarSeccion() {
+        finish();
     }
 }
--- app/src/main/java/activities/Menu_entrenador.java
+++ app/src/main/java/activities/Menu_entrenador.java
@@ -1,17 +1,57 @@
 package activities;
 
 import android.os.Bundle;
-
+import android.view.View;
+import android.widget.Toast;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-
 import com.example.socialab2.R;
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.textview.MaterialTextView;
 
 public class Menu_entrenador extends AppCompatActivity {
+
+    private MaterialButton btnChat, btnEditarPerfilEntrenador, btnMatch, btnCerrarseccion;
+    private MaterialTextView titleText;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.menu_entrenador);
+
+        btnChat = findViewById(R.id.btnChat);
+        btnEditarPerfilEntrenador = findViewById(R.id.btnEditarPerfilEntrenador);
+        btnMatch = findViewById(R.id.btnMatch);
+        btnCerrarseccion = findViewById(R.id.btnCerrarseccion);
+        titleText = findViewById(R.id.titleText);
+
+        btnChat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(Menu_entrenador.this, "Abriendo Chat...", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        btnEditarPerfilEntrenador.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(Menu_entrenador.this, "Abriendo Perfil Entrenador...", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        btnMatch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(Menu_entrenador.this, "Buscando un Match...", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        btnCerrarseccion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(Menu_entrenador.this, "Sección Cerrada.", Toast.LENGTH_SHORT).show();
+                finish();
+            }
+        });
     }
 }
